3aa9b5c28320a9a605b698d748e6547b7a11fe85
Intan Lyeanna <intanlyeanna@192.168.0.29>
2026-02-28 00:09:23 +0800

    remove immunify

--- wp-content/imunify-security/auth.php
+++
@@ -1,5 +0,0 @@
-<?php
-if ( ! defined( 'WPINC' ) ) {
-	exit;
-}
-return json_decode( '{"token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3NzI0Mzg1MDYsInVzZXJuYW1lIjoiYWdkc3BvcnQiLCJzaXRlX3BhdGgiOiIvaG9tZS9hZ2RzcG9ydC9pbnRlcm5hbC5hZ2RzcG9ydHMuY29tIn0.PJdWWYwDTcGgmnrbNTBkA4KU6x_UVg0VNnfJv3Z5DBg"}', true );
